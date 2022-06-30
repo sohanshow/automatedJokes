c297d93e3ac3c7803ac3151b23f77b3a631fab66
sohanshow <sohanshow@gmail.com>
2022-06-30 18:37:25 -0400

    + Production ready 1.3

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-const button = document.getElementById('button')
+const button = document.getElementById('button');
 const audioElement = document.getElementById('audio');
+const touchPad = document.getElementById('container');
+
+
+button.disabled = true;
 
 
 
 // // VoiceRSS Javascript SDK
 const VoiceRSS={speech:function(e){this._validate(e),this._request(e)},_validate:function(e){if(!e)throw"The settings are undefined";if(!e.key)throw"The API key is undefined";if(!e.src)throw"The text is undefined";if(!e.hl)throw"The language is undefined";if(e.c&&"auto"!=e.c.toLowerCase()){var a=!1;switch(e.c.toLowerCase()){case"mp3":a=(new Audio).canPlayType("audio/mpeg").replace("no","");break;case"wav":a=(new Audio).canPlayType("audio/wav").replace("no","");break;case"aac":a=(new Audio).canPlayType("audio/aac").replace("no","");break;case"ogg":a=(new Audio).canPlayType("audio/ogg").replace("no","");break;case"caf":a=(new Audio).canPlayType("audio/x-caf").replace("no","")}if(!a)throw"The browser does not support the audio codec "+e.c}},_request:function(e){var a=this._buildRequest(e),t=this._getXHR();t.onreadystatechange=function(){if(4==t.readyState&&200==t.status){if(0==t.responseText.indexOf("ERROR"))throw t.responseText;audioElement.src=t.responseText,audioElement.play()}},t.open("POST","https://api.voicerss.org/",!0),t.setRequestHeader("Content-Type","application/x-www-form-urlencoded; charset=UTF-8"),t.send(a)},_buildRequest:function(e){var a=e.c&&"auto"!=e.c.toLowerCase()?e.c:this._detectCodec();return"key="+(e.key||"")+"&src="+(e.src||"")+"&hl="+(e.hl||"")+"&r="+(e.r||"")+"&c="+(a||"")+"&f="+(e.f||"")+"&ssml="+(e.ssml||"")+"&b64=true"},_detectCodec:function(){var e=new Audio;return e.canPlayType("audio/mpeg").replace("no","")?"mp3":e.canPlayType("audio/wav").replace("no","")?"wav":e.canPlayType("audio/aac").replace("no","")?"aac":e.canPlayType("audio/ogg").replace("no","")?"ogg":e.canPlayType("audio/x-caf").replace("no","")?"caf":""},_getXHR:function(){try{return new XMLHttpRequest}catch(e){}try{return new ActiveXObject("Msxml3.XMLHTTP")}catch(e){}try{return new ActiveXObject("Msxml2.XMLHTTP.6.0")}catch(e){}try{return new ActiveXObject("Msxml2.XMLHTTP.3.0")}catch(e){}try{return new ActiveXObject("Msxml2.XMLHTTP")}catch(e){}try{return new ActiveXObject("Microsoft.XMLHTTP")}catch(e){}throw"The browser does not support HTTP request"}};
-
 
 
 
@@ -15,24 +18,26 @@ function toggleButton(){
     button.disabled = !button.disabled;
 }
 
-
-
 function intro(){
-
-    VoiceRSS.speech({
-        key: '4455dbfc2ca1472bbc3e0ab0465923cc',
+  VoiceRSS.speech({
+        key: '3e68a590da1d475f87e8591983f2b84e',
         src: 'Hi Charvi. You look tired? Sohan sent me to tell you some jokes. Just click on Generate.',
         hl: 'en-us',
         v: 'Linda',
-        r: 1, 
+        r: 1.5, 
         c: 'mp3',
         f: '44khz_16bit_stereo',
         ssml: false
     });
 
-}
+    if (button.disabled = false)
+       button.disabled = true;
+  
+  };
 
-intro();
+
+
+
 
 
 //Passing Joke to the voice API
@@ -40,7 +45,7 @@ intro();
 function tellMe(joke){
 
     VoiceRSS.speech({
-        key: '4455dbfc2ca1472bbc3e0ab0465923cc',
+        key: '3e68a590da1d475f87e8591983f2b84e',
         src: joke,
         hl: 'en-us',
         v:'Linda',
@@ -79,7 +84,7 @@ async function getJokes(){
           toggleButton(); 
 
 
-    audioElement.addEventListener('ended', toggleButton);
+   
   } catch(error){
 
 
@@ -95,6 +100,11 @@ async function getJokes(){
 
 
 
-
 //Event Listeners
+window.onload = () => {
+    alert('Please touch the robot');
+};
+touchPad.addEventListener('click', intro);
+touchPad.disabled = true;
+audioElement.addEventListener('ended', toggleButton);
 button.addEventListener('click', getJokes);
